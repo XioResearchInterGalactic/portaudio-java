22e5c5f54c7e60d37cf7abd50922d7f1fed75119
Xavier Rubio Jansana <150059896+xrubioj-merlyn@users.noreply.github.com>
2025-06-10 23:06:46 +0200

    Implement Conveyor compatible native loading logic

--- src/main/java/com/portaudio/FileUtils.java
+++ src/main/java/com/portaudio/FileUtils.java
@@ -7,8 +7,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public final class FileUtils {
+    private FileUtils() { /* no init */ }
+
+    static Logger logger = Logger.getLogger(FileUtils.class.getCanonicalName());
+
     static Path dir = null;
 
     static private Path getTempDirectory() {
@@ -28,16 +33,35 @@ public final class FileUtils {
     //  We're keeping this for development, but for production we're switching to asking Conveyor
     //  to extract all the libraries to make the app slightly faster and reduce runtime errors.
     static void load(String resource) {
+
+        String libraryNameWithExtension = new File(resource).getName();
+        String libraryName;
+        if (libraryNameWithExtension.contains("."))
+            libraryName = libraryNameWithExtension.substring(0, libraryNameWithExtension.lastIndexOf('.'));
+        else
+            libraryName = libraryNameWithExtension;
+
+        String appDir = System.getProperty("app.dir");
+        if (appDir != null) {
+            // && means app.dir
+            // Windows: -Djava.library.path=&&;&&..\bin
+            // macOS:   -Djava.library.path=&&/../runtime/Contents/Home/lib:&&
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("win")) {
+                if (tryLoadLibrary(appDir, libraryNameWithExtension)) return;
+                if (tryLoadLibrary(appDir, "..\\bin", libraryNameWithExtension)) return;
+            } else {
+                if (tryLoadLibrary(appDir, "../runtime/Contents/Home/lib", libraryNameWithExtension)) return;
+                if (tryLoadLibrary(appDir, libraryNameWithExtension)) return;
+            }
+        }
+
         try {
-            String libraryNameWithExtension = new File(resource).getName();
-            String libraryName;
-            if (libraryNameWithExtension.contains("."))
-                libraryName = libraryNameWithExtension.substring(0, libraryNameWithExtension.lastIndexOf('.'));
-            else
-                libraryName = libraryNameWithExtension;
             System.loadLibrary(libraryName);
+            return;
         } catch (Throwable t) {
             // Ignore the error, fall back to loading from resource (used in development)
+            logger.warning("Error loading " + libraryName + ". Will try to extract it from resources.");
         }
 
         try {
@@ -49,5 +73,26 @@ public final class FileUtils {
         } catch (Throwable t) {
             throw new RuntimeException(t);
         }
+    }
+
+    private static boolean tryLoadLibrary(String... pathElements) {
+        File file = null;
+        for (String pathElement : pathElements) {
+            if (file == null)
+                file = new File(pathElement);
+            else
+                file = new File(file, pathElement);
+        }
+        if (file == null) {
+            logger.severe("Error loading library as the absolute path is null. This was absolutely unexpected and is probably due to bad application code. May continue trying with other paths.");
+            return false;
+        }
+        try {
+            System.load(file.getAbsolutePath());
+            return true;
+        } catch (Throwable ex) {
+            logger.warning("Error loading " + file.getAbsolutePath() + ". May continue trying with other paths.");
+        }
+        return false;
     }
 }
